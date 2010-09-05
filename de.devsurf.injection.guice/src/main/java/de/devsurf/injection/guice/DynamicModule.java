bc914a63ef4c4be1cb8619d1a79d5f4b3edb2a55
Daniel Manzke <daniel.manzke@googlemail.com>
2010-09-05 21:48:52 +0200

    added license headers - Apache License 2.0

--- de.devsurf.injection.guice/src/main/java/de/devsurf/injection/guice/DynamicModule.java
+++ de.devsurf.injection.guice/src/main/java/de/devsurf/injection/guice/DynamicModule.java
@@ -1,15 +1,31 @@
+/*******************************************************************************
+ * Copyright 2010, Daniel Manzke
+ * 
+ * Licensed under the Apache License, Version 2.0 (the "License"); 
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ * 
+ *       http://www.apache.org/licenses/LICENSE-2.0
+ * 
+ * Unless required by applicable law or agreed to in writing, 
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND,
+ * either express or implied. See the License for the specific language 
+ * governing permissions and limitations under the License.
+ * 
+ ******************************************************************************/
 package de.devsurf.injection.guice;
 
 import com.google.inject.Module;
 
 /**
- * Due the fact that Guice is not able to bind Modules to {@link Module},
- * we have to bypass this issue. Due that fact we mark all Modules, which should
- * be injected and bind them.
+ * Due the fact that Guice is not able to bind Modules to {@link Module}, we
+ * have to bypass this issue. Due that fact we mark all Modules, which should be
+ * injected and bind them.
  * 
  * @author Daniel Manzke
- *
+ * 
  */
-public interface DynamicModule extends Module{
+public interface DynamicModule extends Module {
 
 }
--- de.devsurf.injection.guice/src/main/java/de/devsurf/injection/guice/scanner/ScannerModule.java
+++ de.devsurf.injection.guice/src/main/java/de/devsurf/injection/guice/scanner/ScannerModule.java
@@ -1,3 +1,19 @@
+/*******************************************************************************
+ * Copyright 2010, Daniel Manzke
+ * 
+ * Licensed under the Apache License, Version 2.0 (the "License"); 
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ * 
+ *       http://www.apache.org/licenses/LICENSE-2.0
+ * 
+ * Unless required by applicable law or agreed to in writing, 
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND,
+ * either express or implied. See the License for the specific language 
+ * governing permissions and limitations under the License.
+ * 
+ ******************************************************************************/
 package de.devsurf.injection.guice.scanner;
 
 import java.io.IOException;
@@ -11,9 +27,9 @@ import de.devsurf.injection.guice.annotations.GuiceModule;
 
 /**
  * The ScannerModule will be injected with a ClasspathScanner and
- * the needed Annotation Listeners will be added. The Module adds 
- * all Modules with {@link GuiceModule} and all Beans annotated 
- * with {@link AutoBind}.
+ * the needed Annotation Listeners will be added. The attached Listeners
+ * will install all Modules annotated with {@link GuiceModule} and bind 
+ * all Beans annotated with {@link AutoBind}.
  * 
  * @author Daniel Manzke
  *
